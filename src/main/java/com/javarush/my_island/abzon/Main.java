06f745f8aa8ee30422f97b748313495bcd45bf47
VorotillA <dima.abzon@gmail.com>
2023-08-13 21:35:38 +0300

    save commit

---
+++ src/main/java/com/javarush/my_island/abzon/Main.java
@@ -0,0 +1,9 @@
+package com.javarush.my_island.abzon;
+
+
+
+public class Main {
+    public static void main(String[] args)  {
+        System.out.println("tdhdfhdfg");
+    }
+}
---
+++ src/main/java/com/javarush/my_island/abzon/nature/Nature.java
@@ -0,0 +1,50 @@
+package com.javarush.my_island.abzon.nature;
+
+public class Nature {
+    // Природа
+    public static class Plant { // Растение
+
+    }
+
+    public static class Animal { // Животные
+        public static class Predatory { //      Хищьные
+            public static class Wolf { //Волк
+
+            }
+
+            public static class Boa {} //Удав
+
+            public static class Fox {}//лиса
+
+            public static class Bear {}// медведь
+
+            public static class Eagle {}// орел
+
+        }
+
+        public static class Herbivore { //    Травоядные
+            public static class Hourse {}//лошадь
+
+            public static class Deer {}// олень
+
+            public static class Rabit {}//кролик
+
+            public class Mouse {}// мышь
+
+            public class Goat {}//коза
+
+            public class Sheep {}//овца
+
+            public class boar {}//кабан
+
+            public class Buffalo{} //буйвол
+
+            public class Duck {}//утка
+
+            public class Caterpillar{} //гусеница
+
+        }
+
+
+    }
+}
